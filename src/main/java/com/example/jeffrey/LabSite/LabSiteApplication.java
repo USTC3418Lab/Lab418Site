6b90ed24735c55c36ee083595b447913faaf58de
eric <eric@localhost.localdomain>
2020-07-28 06:39:02 -0400

    init

---
+++ src/main/java/com/example/jeffrey/LabSite/LabSiteApplication.java
@@ -0,0 +1,13 @@
+package com.example.jeffrey.LabSite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabSiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabSiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jeffrey/LabSite/LabSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jeffrey.LabSite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabSiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
