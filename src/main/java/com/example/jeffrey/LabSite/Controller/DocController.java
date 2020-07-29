5faa6b22ad567c7ec236201a1e891f61ca5e9cea
15205648876 <1710085142@qq.com>
2020-07-29 16:41:36 +0800

    完成接口：`/doc?title={}`查询所有或者单篇文章，由传入的参数决定

--- src/main/java/com/example/jeffrey/LabSite/Controller/DocController.java
+++ src/main/java/com/example/jeffrey/LabSite/Controller/DocController.java
@@ -3,6 +3,7 @@ package com.example.jeffrey.LabSite.Controller;
 import com.example.jeffrey.LabSite.Entity.DocEntity;
 import com.example.jeffrey.LabSite.Mapper.DocMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,15 +15,14 @@ public class DocController {
     @Autowired
     private DocMapper docMapper;
 
-    @RequestMapping("/doc/get")
-    public String getdoc(String title){
-        DocEntity docEntity = docMapper.getDocByTitle(title);
-        return docEntity.toString();
+    @RequestMapping("/doc?{title}")
+    public DocEntity getdoc(@PathVariable("title") String title){
+        return docMapper.getDocByTitle(title);
     }
 
     @RequestMapping("/doc")
-    public String getdocs(){
+    public List<DocEntity> getdocs(String title){
         List<DocEntity> list = docMapper.getDocs();
-        return docMapper.getDocs().toString();
+        return docMapper.getDocs();
     }
 }
